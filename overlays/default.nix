5aacf1c0fbe26ce29ad7ad77d71dcb1c13725d4a
PerchunPak <git@perchun.it>
2024-08-02 00:53:28 +0200

    Remove nixpkgs-unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,5 @@
 # This file defines overlays
-{inputs, ...}: {
+{...}: {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
 
@@ -10,14 +10,5 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-  };
-
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
   };
 }
