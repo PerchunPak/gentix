1bd2cd6c017b1b70e592f3a25092183a452beb39
PerchunPak <git@perchun.it>
2024-08-02 12:26:20 +0200

    Disable ZFS
    It is broken on latest kernel

--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # ZFS is broken on new kernel
+  boot.supportedFilesystems.zfs = lib.mkForce false;
 }
